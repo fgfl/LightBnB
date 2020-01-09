53a785cc23741d2191f263bd91c30b6d5dbc5ad2
Frederick <frederick.gfl@gmail.com>
2020-01-09 19:48:45 +0000

    Update addUser to add data to psql
    - returns the user object in promise to login immediately

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -60,10 +60,19 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  const addUserQuery = `
+    INSERT INTO users (name, email, password)
+      VALUES ($1, $2, $3)
+      RETURNING id, name, email, password;
+  `;
+  const value = [user.name, user.email, user.password];
+
+  return (
+    pool
+      .query(addUserQuery, value)
+      .then((res) => res.rows[0])
+  );
+
 }
 exports.addUser = addUser;
 
