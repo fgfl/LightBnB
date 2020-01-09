0b6b3a9e7d2114e78ac9ecc6d7d65b46010ca271
Frederick <frederick.gfl@gmail.com>
2020-01-09 01:13:00 +0000

    Add 3 new queries
    - get user details
    - get avg reservation duration
    - get all property list by city. Include avg rating

---
+++ 1_queries/03_propery_listings_by_city.sql
@@ -0,0 +1,7 @@
+SELECT properties.*, AVG(property_reviews.rating) AS average_rating
+  FROM properties 
+    JOIN property_reviews ON property_reviews.property_id = properties.id
+  WHERE city LIKE '%Vancouver%'
+  GROUP BY properties.id
+  HAVING AVG(property_reviews.rating) >= 4
+  ORDER BY cost_per_night ASC
